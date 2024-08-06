6e0bb7041d3bccc42e9a62a8f02dee8bccedc0d5
Danny Isaac <danny.isaac@klarna.com>
2024-08-07 01:00:22 +0200

    feat(nvim): add some new git plugins

---
+++ nvim/lua/plugins/git-plugins.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua", -- optional
+    },
+    config = true,
+  },
+  {
+    "tpope/vim-fugitive",
+    vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Git blame for current file (leader + Git Blame)" }),
+    vim.keymap.set("n", "<leader>gs", ":Git status<CR>", { desc = "Git status" }),
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function() require("gitsigns").setup() end,
+
+    vim.keymap.set(
+      "n",
+      "<leader>gp",
+      ":Gitsigns preview_hunk<CR>",
+      { desc = "Preview hunk for current line (leader + Git Preview)" }
+    ),
+  },
+}
