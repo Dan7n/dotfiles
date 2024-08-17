6b6661cb1490db62806e2e241655eafd67f7e911
Danny Isaac <danny95.nbl@gmail.com>
2024-08-18 00:07:22 +0200

    FINALLY fixed wezterm behaving weirdly on a swedish keyboard!

--- wezTerm/.wezterm.lua
+++ wezTerm/.wezterm.lua
@@ -5,4 +5,7 @@ config.color_scheme = "Catppuccin Mocha (Gogh)"
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 13.5
 
+config.use_ime = false
+config.send_composed_key_when_left_alt_is_pressed = true
+
 return config
