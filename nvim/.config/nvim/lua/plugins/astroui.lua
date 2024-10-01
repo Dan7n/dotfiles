17d0140d545a7864354f53c80ba1c44c2941e294
Danny Isaac <danny.isaac@klarna.com>
2024-10-02 00:21:46 +0200

    feat: update tmux, nvim, and kitten colorschemes

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -12,7 +12,7 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "catppuccin-mocha",
-    colorscheme = "catppuccin-frappe",
+    colorscheme = "nightfox",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
